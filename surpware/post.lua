cf28a08571b6794ee122eaab531e452d53659611
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-02-09 13:26:05 -0800

    Update post.lua

--- surpware/post.lua
+++ surpware/post.lua
@@ -1,6 +1,3 @@
-if request then
-	local components = (...)
-	request({Url=components["url"], Method="POST", Body=game:GetService("HttpService"):JSONEncode(components["body"])})
-else
-	warn("Your executor is too shitty!")
-end
+local components = (...)
+print(components["body"])
+request({Url=components["url"], Method="POST", Body=game:GetService("HttpService"):JSONEncode(components["body"])})
