9177bcdf5ddc33d9c23f5f57b37c2f3f3f10d793
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-02-09 12:05:55 -0800

    Update post.lua

--- surpware/post.lua
+++ surpware/post.lua
@@ -1,8 +1,12 @@
-local components = (...)
-if components["url"] and components["body"] then
-  pcall(function()
-		request({Url=components["url"], Method="POST", Body=game:GetService("HttpService"):JSONEncode(components["body"])})
-	end)
+if request then
+	local components = (...)
+	if components["url"] and components["body"] then
+	  pcall(function()
+			request({Url=components["url"], Method="POST", Body=game:GetService("HttpService"):JSONEncode(components["body"])})
+		end)
+	else
+	  warn("Invalid components")
+	end
 else
-  warn("Invalid components")
+	warn("Your executor is too shitty!")
 end
