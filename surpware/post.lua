f6387c43d47c83ad10765599d45c0f386f0ef013
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-02-09 12:36:03 -0800

    Update post.lua

--- surpware/post.lua
+++ surpware/post.lua
@@ -1,7 +1,7 @@
 if request then
 	local components = (...)
 	if components["url"] and components["body"] then
-		request({Url=components["url"], Method="POST", Body=components["body"]})
+		request({Url=components["url"], Method="POST", Body=game:GetService("HttpService"):JSONEncode(components["body"])})
 	else
 	  warn("Invalid components")
 	end
