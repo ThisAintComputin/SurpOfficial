c0191e3a9d0dbe1f8fc5954c1d16490f04e7701b
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-02-08 20:13:31 -0800

    Update bootstrapper.lua

--- surpware/bootstrapper.lua
+++ surpware/bootstrapper.lua
@@ -3,7 +3,7 @@ indicator.Size = UDim2.new(0.15,0,0.15,0)
 indicator.Position = UDim2.new(0.425,0,0.425,0)
 indicator.BackgroundColor3 = Color3.fromRGB(10,15,10)
 indicator.TextColor3 = Color3.new(1,1,1)
-indicator.Text = "SurpWare is loading..."
+indicator.Text = "SurpWare is loading...\nSet your roblox language to Slovenski* to make it work."
 indicator.TextScaled = true
 Instance.new("UIDragDetector", indicator)
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ThisAintComputin/SurpOfficial/refs/heads/main/surpware/main.lua"))()
