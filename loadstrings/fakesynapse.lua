79cb03a1dd44ac6c0ea7c2493367dbe8d5847cf4
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-03-08 14:08:08 -0800

    Update fakesynapse.lua

--- loadstrings/fakesynapse.lua
+++ loadstrings/fakesynapse.lua
@@ -55,9 +55,8 @@ task.spawn(function()
 end)
 
 getgenv().identifyexecutor = function() return table.unpack{fake_executor_name, "1.0.0"} end
-getgenv().http = {request = (syn.request or request)}
 getgenv().syn = {request = (http_request or request)}
+getgenv().http = {request = (syn.request or request)}
 getgenv().getthreadidentity = function() return fake_identity end
 getgenv().printidentity = function() print("Current identity is "..tostring(fake_identity)) end
-if script.Parent then script.Parent.Name = fake_executor_name end
 if script.Parent then script.Parent.Name = fake_executor_name end
