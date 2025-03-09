998613c0ea03821ae6b00115e47b8c69718f7d37
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-03-08 17:05:06 -0800

    Create antihttpspy.lua

---
+++ loadstrings/antihttpspy.lua
@@ -0,0 +1,14 @@
+-- Usage:
+-- local lib = loadstring(game:HttpGet(""))()
+-- local game.HttpGet = lib.HttpGet
+-- local game.HttpPost = lib.HttpPost
+-- OR you could do the recommended method:
+-- (code)
+-- key = lib:HttpGet(verysecretkeylink)
+
+if newcclosure and getgenv and request then
+    local genv = newcclosure(function() return getgenv() end)
+    return {HttpGet = function(url) return genv().request({Method="GET", Url=url}).Body end, HttpPost = function(url, data) return genv().request({Method="POST", Url=url, Body=data}).Body end}
+else
+    error("Your executor is not supported by Anti-HttpSpy!")
+end
