fb405476068144f1a5db2b657ab67f24c26705c5
ThisAintComputin <142166632+ThisAintComputin@users.noreply.github.com>
2025-03-17 19:53:10 -0700

    Update main.lua

--- surpware/main.lua
+++ surpware/main.lua
@@ -1,4 +1,4 @@
 if not getgenv().surpwareCachedScript then
   getgenv().surpwareCachedScript = game:HttpGet("https://raw.githubusercontent.com/ThisAintComputin/SurpOfficial/refs/heads/main/surpware/scriptcache.lua")
 end
-loadstring(getgenv().surpwareCachedScript)()
+loadstring(getgenv().surpwareCachedScript)(...)
